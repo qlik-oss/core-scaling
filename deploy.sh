84f92b62d7a57c69287552bca024fffc3582bcb6
Henrik Carlioth <hca@qlik.com>
2018-04-04 17:02:42 +0200

    change name of clusterrole

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,8 @@ cd "$(dirname "$0")"
 set -e
 
 kubectl create -f ./namespaces.yaml
-kubectl create clusterrolebinding myname-cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud info | grep Account |  awk -F'[][]' '{print $2}')
+kubectl create clusterrolebinding cluster-admin-binding --clusterrole=cluster-admin --user=$(gcloud info | grep Account |  awk -F'[][]' '{print $2}')
 kubectl create -f ./prometheus
 kubectl create -f ./custom-metrics-api
 kubectl create -f ./ingress
 kubectl create -f ./rbac-config.yaml
-
-# Add Dockhub credentials
-# Deploy core.
