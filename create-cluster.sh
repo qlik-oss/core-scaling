9b6773e0d53da1769c0429759626782971628a99
Henrik Carlioth <carlioth@users.noreply.github.com>
2018-04-29 20:11:38 +0200

    Change session placement, convinience scripts (#27)
    * Convinience scripts, new session placement, bum engine
    
    * Disk command fix, and remove dashboard
    
    * Remove double k8 version setting

--- create-cluster.sh
+++ create-cluster.sh
@@ -11,5 +11,7 @@ gcloud container --project $GCLOUD_PROJECT clusters create $K8S_CLUSTER \
   --disk-size $GCLOUD_DISK_SIZE --scopes $GCLOUD_SCOPES --num-nodes $GCLOUD_NUM_NODES \
   --network "default" --enable-cloud-logging --enable-cloud-monitoring \
   --subnetwork "default" --enable-autoscaling --min-nodes $GCLOUD_MIN_NODES \
-  --max-nodes $GCLOUD_MAX_NODES --addons KubernetesDashboard &&
-gcloud compute disks create --size=10GB --zone=$GCLOUD_ZONE app-nfs-disk
+  --max-nodes $GCLOUD_MAX_NODES &&
+gcloud compute disks create --size=10GB --zone=$GCLOUD_ZONE app-nfs-disk &&
+gcloud container node-pools create monitoring --cluster=$K8S_CLUSTER \
+  --machine-type=$GCLOUD_MACHINE_TYPE --num-nodes=1
