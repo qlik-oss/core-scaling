76689e7911a2c83312a43e32600d67f9c957bae7
Henrik Carlioth <carlioth@users.noreply.github.com>
2018-07-11 13:14:04 +0200

    Update app to enable abac structure. Readonly enabled (#53)

--- deployment-test/verify-deployment.spec.js
+++ deployment-test/verify-deployment.spec.js
@@ -10,7 +10,7 @@ describe('Verify the Deployment', () => {
   beforeEach(async () => {
     session = enigma.create({
       schema,
-      url: `ws://${testClusterIP}/app/doc/Shared-Africa-Urbanization.qvf`,
+      url: `ws://${testClusterIP}/app/doc/5f5f03fe-00f3-4a4b-ac52-db32152a7e44`,
       createSocket: url => new WebSocket(url),
     });
     qix = await session.open();
@@ -23,13 +23,22 @@ describe('Verify the Deployment', () => {
   it('Verify that the correct app is opened', async () => {
     const app = await qix.getActiveDoc();
     const layout = await app.getAppLayout();
-    expect(layout.qTitle).to.equal('Shared-Africa-Urbanization');
+    expect(layout.qTitle).to.equal('5f5f03fe-00f3-4a4b-ac52-db32152a7e44');
   });
 
-  it('Verify that the app includes at least one of the correct objects', async () => {
+  it('Verify that the app includes at least one of the correct fields', async () => {
     const app = await qix.getActiveDoc();
-    const obj = await app.getObject('PPPZWVQ');
-    const objLayout = await obj.getLayout();
-    expect(objLayout.title).to.equal('Average life expectancy (years)');
+    const landField = await app.getFieldDescription('Land Area');
+    expect(landField.qName).to.equal('Land Area');
+  });
+
+  it('Verify that reload is not possible', async () => {
+    const app = await qix.getActiveDoc();
+    try {
+      const result = await app.doReload();
+      throw new Error("Reload success");
+    } catch (err) {
+      expect(err.message).to.equal('Access denied');
+    }
   });
 });
