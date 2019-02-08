239b39451e3d7f7e4e7f866e7108ddf6645e5b57
Henrik Carlioth <hca@qlik.com>
2019-02-08 13:08:45 +0100

    update tests

--- deployment-test/verify-deployment.spec.js
+++ deployment-test/verify-deployment.spec.js
@@ -10,7 +10,7 @@ describe('Verify the Deployment', () => {
   beforeEach(async () => {
     session = enigma.create({
       schema,
-      url: `ws://${testClusterIP}/app/doc/5f5f03fe-00f3-4a4b-ac52-db32152a7e44`,
+      url: `ws://${testClusterIP}/app/doc/739db838-dd28-4078-8715-ee9cfcc06c29`,
       createSocket: url => new WebSocket(url),
     });
     qix = await session.open();
@@ -23,7 +23,7 @@ describe('Verify the Deployment', () => {
   it('Verify that the correct app is opened', async () => {
     const app = await qix.getActiveDoc();
     const layout = await app.getAppLayout();
-    expect(layout.qTitle).to.equal('5f5f03fe-00f3-4a4b-ac52-db32152a7e44');
+    expect(layout.qTitle).to.equal('739db838-dd28-4078-8715-ee9cfcc06c29');
   });
 
   it('Verify that the app includes at least one of the correct fields', async () => {
